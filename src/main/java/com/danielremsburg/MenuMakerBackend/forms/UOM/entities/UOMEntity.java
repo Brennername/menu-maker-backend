d943036da41e091e9d14b9b6d113e3d8eae25627
danielremsburg <danielremsburg@gmail.com>
2024-10-04 15:29:25 +0000

    added UOMEntity

--- src/main/java/com/danielremsburg/MenuMakerBackend/forms/UOM/entities/UOMEntity.java
+++ src/main/java/com/danielremsburg/MenuMakerBackend/forms/UOM/entities/UOMEntity.java
@@ -1,5 +1,18 @@
 package com.danielremsburg.MenuMakerBackend.forms.UOM.entities;
-
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "units_of_measure")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UOMEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY
+)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
 }
